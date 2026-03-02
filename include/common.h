e1344d4dcb46891afaa928e514e6ecd4f59f0e71
YANG <yanson0557@gmail.com>
2026-03-02 17:25:19 +0800

    chore: 更新学生信息格式

--- include/common.h
+++ include/common.h
@@ -9,7 +9,7 @@
 #include <string.h>
 
 #define SYSTEM_NAME "计费管理系统"
-#define STUDENT_INFO "1023007337-杨立晗-软件2301"
+#define STUDENT_INFO "37-1023007337-杨立晗-软件2301"
 
 #define INPUT_BUF_SIZE 128
 #define MENU_MIN 0
