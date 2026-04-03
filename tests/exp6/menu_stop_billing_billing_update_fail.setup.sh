fc16e427e9563b2d3093e0cbdda9aef1ec58e16d
YANG <yanson0557@gmail.com>
2026-04-04 01:00:23 +0800

    test: 优化测试6的逻辑

--- tests/exp6/menu_stop_billing_billing_update_fail.setup.sh
+++ tests/exp6/menu_stop_billing_billing_update_fail.setup.sh
@@ -1,11 +1,13 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+start_time="$(date -d '20 seconds ago' '+%Y-%m-%d %H:%M:%S')"
+
 mkdir -p data
 cat > data/cards.txt <<'DATA'
 stopA7|pwd001|1|2026-01-01 00:00:00|2027-01-01 00:00:00|0|2026-01-01 00:00:00|0|10000|0
 DATA
-cat > data/billings.txt <<'DATA'
-stopA7|2026-03-27 10:00:00|0|0|0|0
+cat > data/billings.txt <<DATA
+stopA7|${start_time}|0|0|0|0
 DATA
 chmod 444 data/billings.txt
